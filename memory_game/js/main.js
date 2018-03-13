a43dccd5a826c74656c339bc10258eca1b030690
Roselou <34549947+Roselou@users.noreply.github.com>
2018-03-12 21:03:23 -0700

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,37 +27,62 @@ var checkForMatch = function () {
 		console.log("Sorry, try again.");
 	}
 };
+
+var cardsInPlay = [];
+
 var flipCard = function () {
 	var cardId = this.getAttribute('data-id');
-	var cardId = cards[0, 1, 2, 3]
+	// var cardId = cards[0, 1, 2, 3]
 	console.log("User flipped " + cards[cardId.rank]);
-var cardsInPlay = [];
-cardsInPlay.push(cards[cardId.rank]);
-console.log("User flipped queen");
-console.log("User flipped King");
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+cardsInPlay.push(cards[cardId]);
+// console.log("User flipped queen");
+// console.log("User flipped King");
+// console.log(cards[cardId].cardImage);
+// console.log(cards[cardId].suit);
 var cardId = this.setAttribute('src', cards[cardId].cardImage);
-if (cardsInPlay.length === 2) {
-	cards[0] === cards[1]
-	alert("You found a match!")
-    } else if (cardsInPlay.length === 2) {
-		cards[2] ===  cards[3]
-		alert("You found a match!")
-	} else (cardsInPlay.length === 2) 
-	{
-			alert("Sorry, try again.")
+	// if (cardsInPlay.length === 2) {
+	// cards[0] === cards[1]
+	// alert("You found a match!")
+ //    } else if (cardsInPlay.length === 2) {
+	// 	cards[2] ===  cards[3]
+	// 	alert("You found a match!")
+	// } else (cardsInPlay.length === 2) 
+	// {
+	// 		alert("Sorry, try again.")
+	// 	}
+
+	// check if I have 2 cards to compare
+	// if I do, i will check if they are the same
+	// if they are the same, say "match!"
+	// if they are not the same, say "not a match!"
+
+	console.log(cardsInPlay)
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+			alert ("You found a match!")
+			cardsInPlay = [];
+		} else {
+			alert ("Sorry, try again")
 		}
+	    // if (cardsInPlay[2] === cardsInPlay[3]) {
+	    // 	alert ("You found a match!")
+	    // };
+	}
+
 };
-flipCard(checkForMatch); 
-var createBoard = function() {
+// var flipCard = function(checkForMatch) {};
+
+var createBoard = function() {	
 for (var i = 0; i < cards.length; i++) {
-cardElement.addEventListener('click', flipcard).appendChild(game-board);
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', "images/back.png");
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById("game-board").appendChild(cardElement);
+// document.querySelector('img').appendChild("game-board");
 }
 };
-var cardElement = document.createElement('img');
-cardElement.setAttribute('src', 'images/bacl.png');
-cardElement.setAttribute('data-id', i);
+
 createBoard();
 
 
